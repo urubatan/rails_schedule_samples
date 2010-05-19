ed9602f29278ae3e0332e96ba6be3d79e8d282be
Rodrigo Urubatan Ferreira Jardim <rodrigo@urubatan.com.br>
2010-05-19 17:45:36 -0300

    removed unused directory

--- openwfe_sample/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = '015d3e631e19edb99302e8c1fff708984c829b3f49204510ebd691ef569f027487c91d07a6077bfe8f56f7206821d8c66d06c89862b0b151307fdb9dde5e5f44'
